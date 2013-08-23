cbece389dee4a430d8883a49c521c319312a5666
Roman Dolgiy <roman@bravetstudio.com>
2013-08-23 19:39:24 +0300

    Update

--- postinstall.sh
+++ postinstall.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-mkdir -p ~/data/shared
-mv -n ~/current/var ~/data/shared
-mv -n ~/current/media ~/data/shared
-mv -n ~/current/downloader/.cache ~/data/shared/downloader
-mv -n ~/current/app/etc/local.xml ~/data/shared/app/etc/local.xml
-mv -n ~/current/downloader/cache.cfg ~/data/shared/downloader
-mv -n ~/current/downloader/connect.cfg ~/data/shared/downloader
+mkdir -p ~/data/shared/app/etc
+mkdir -p ~/data/shared/downloader
+cp -R -L -n ~/current/var ~/data/shared
+cp -R -L -n ~/current/media ~/data/shared
+cp -R -L -n ~/current/downloader/.cache ~/data/shared/downloader/.cache
+cp -R -L -n ~/current/app/etc/local.xml ~/data/shared/app/etc/local.xml
+cp -R -L -n ~/current/downloader/cache.cfg ~/data/shared/downloader
+cp -R -L -n ~/current/downloader/connect.cfg ~/data/shared/downloader
 ln -s ~/data/shared/var ~/current
 ln -s ~/data/shared/media ~/current
-ln -s ~/data/shared/downloader/.cache ~/current/downloader
 ln -s ~/data/shared/app/etc/local.xml ~/current/app/etc
 ln -s ~/data/shared/downloader/cache.cfg ~/current/downloader
 ln -s ~/data/shared/downloader/connect.cfg ~/current/downloader
