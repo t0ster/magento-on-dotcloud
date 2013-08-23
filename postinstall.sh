6b24f39307661f918d2129762985858f5a2c140c
Roman Dolgiy <roman@bravetstudio.com>
2013-08-23 19:50:37 +0300

    Update

--- postinstall.sh
+++ postinstall.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 mkdir -p ~/data/shared/app/etc
 mkdir -p ~/data/shared/downloader
-cp -R -L -n ~/current/var ~/data/shared
-cp -R -L -n ~/current/media ~/data/shared
-cp -R -L -n ~/current/downloader/.cache ~/data/shared/downloader/.cache
-cp -R -L -n ~/current/app/etc/local.xml ~/data/shared/app/etc/local.xml
-cp -R -L -n ~/current/downloader/cache.cfg ~/data/shared/downloader
-cp -R -L -n ~/current/downloader/connect.cfg ~/data/shared/downloader
+cp -R -L -n ~/current/var ~/data/shared && rm ~/current/var
+cp -R -L -n ~/current/media ~/data/shared && rm ~/current/media
+cp -R -L -n ~/current/downloader/.cache ~/data/shared/downloader/.cache && rm ~/current/downloader/.cache
+cp -R -L -n ~/current/app/etc/local.xml ~/data/shared/app/etc/local.xml && rm ~/current/app/etc/local.xml
+cp -R -L -n ~/current/downloader/cache.cfg ~/data/shared/downloader && ~/current/downloader/cache.cfg
+cp -R -L -n ~/current/downloader/connect.cfg ~/data/shared/downloader && ~/current/downloader/connect.cfg
 ln -s ~/data/shared/var ~/current
 ln -s ~/data/shared/media ~/current
 ln -s ~/data/shared/app/etc/local.xml ~/current/app/etc
